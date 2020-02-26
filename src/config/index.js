21c1f558fbafd2904f0302b1f9d2c5cf0e6795c0
Kamil <kamilwronka7@gmail.com>
2020-02-26 17:04:45 +0100

    added config

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  JWT_SECRET: process.env.JWT_SECRET || "sadasdasdasdasd",
+  DOMAIN_NAME: process.env.DOMAIN_NAME || "localhost",
+  PAGE_URL: process.env.PAGE_URL || "http://localhost:3000"
+};
